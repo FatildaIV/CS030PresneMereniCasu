79ddaa53e099d032f52ba04e232c392ba2531e21
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 13:05:59 +0200

    Initial commit

---
+++ CS030PresneMereniCasu/CS030PresneMereniCasu/PresneMereniCasuForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace CS030PresneMereniCasu
+{
+    partial class PresneMereniCasuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testButton = new System.Windows.Forms.Button();
+            this.vystupTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.testButton.Location = new System.Drawing.Point(287, 67);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(241, 107);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "Spustit Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // vystupTextBox
+            // 
+            this.vystupTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.vystupTextBox.Location = new System.Drawing.Point(12, 222);
+            this.vystupTextBox.Multiline = true;
+            this.vystupTextBox.Name = "vystupTextBox";
+            this.vystupTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.vystupTextBox.Size = new System.Drawing.Size(776, 216);
+            this.vystupTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vystupTextBox);
+            this.Controls.Add(this.testButton);
+            this.Name = "Form1";
+            this.Text = "CS030 Přesné Měření Času - Daniel Černý 3.D";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.TextBox vystupTextBox;
+    }
+}
+
---
+++ CS030PresneMereniCasu/CS030PresneMereniCasu/PresneMereniCasuForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CS030PresneMereniCasu
+{
+    public partial class PresneMereniCasuForm : Form
+    {
+        public PresneMereniCasuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            StopkyTestDateTime();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+            StopkyTestHighResolutionDateTime();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+            StopkyTestStopwatchHashSet();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+            StopkyTestStopwatch();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+            StopkyTestStopwatchDrift();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+        }
+
+        private void StopkyTestDateTime() {
+
+            vystupTextBox.AppendText("Testing DateTime for 1 seconds...");
+
+            var distinctValues = new HashSet<DateTime>();
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 1)
+            {
+                distinctValues.Add(DateTime.UtcNow);
+            }
+
+            sw.Stop();
+
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(
+                string.Format(
+                    "Precision: {0:0.000000} ms ({1} samples)",
+                    sw.Elapsed.TotalMilliseconds / distinctValues.Count,
+                    distinctValues.Count));
+
+        }
+
+        private void StopkyTestHighResolutionDateTime()
+        {
+
+            vystupTextBox.AppendText("Testing High Resolution DateTime for 1 seconds...");
+
+            var distinctValues = new HashSet<DateTime>();
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 1)
+            {
+                distinctValues.Add(HighResolutionDateTime.UtcNow);
+            }
+
+            sw.Stop();
+
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(
+                string.Format(
+                    "Precision: {0:0.000000} ms ({1} samples)",
+                    sw.Elapsed.TotalMilliseconds / distinctValues.Count,
+                    distinctValues.Count));
+
+        }
+
+        private void StopkyTestStopwatchHashSet()
+        {
+
+            vystupTextBox.AppendText("Testing Stopwatch for 1 seconds...");
+
+            var distinctValues = new HashSet<long>();
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 1)
+            {
+                distinctValues.Add(sw.ElapsedTicks);
+            }
+
+            sw.Stop();
+
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(
+                string.Format(
+                    "Resolution: {0:0.000000} ms ({1} samples)",
+                    sw.Elapsed.TotalMilliseconds / distinctValues.Count,
+                    distinctValues.Count));
+
+        }
+
+        private void StopkyTestStopwatch()
+        {
+
+            vystupTextBox.AppendText("Testing Stopwatch for 1 seconds...");
+
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 1)
+            {
+                
+            }
+
+            sw.Stop();
+
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(
+                string.Format(
+                    "Resolution: {0:0.000000} ms ({1} samples)",
+                    sw.Elapsed.TotalMilliseconds / sw.ElapsedTicks,
+                    sw.ElapsedTicks));
+
+        }
+
+        private void StopkyTestStopwatchDrift() {
+            var start = HighResolutionDateTime.UtcNow;
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 600)
+            {
+                DateTime nowBasedOnStopwatch = start + sw.Elapsed;
+                TimeSpan diff = HighResolutionDateTime.UtcNow - nowBasedOnStopwatch;
+
+                vystupTextBox.AppendText(string.Format("Stopwatch to UTC drift: {0:0.000} ms", diff.TotalMilliseconds));
+                vystupTextBox.AppendText(Environment.NewLine);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
